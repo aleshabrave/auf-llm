9a6f58022df8ffeb0aa45fa9bf774eb6a463b025
a.smelik <a.smelik@tinkoff.ru>
2024-05-17 12:15:48 +0500

    lol

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from framework.tasks.cyclomatic_complexity import case_results, reports
 
-#case_results.execute_case_results()
-#reports.make_total()
-#reports.make_relate("yandex_gpt_lite_code_w_sexp_ast_python", "yandex_gpt_lite_only_code_python")
-#reports.make_relate("yandex_gpt_lite_code_w_sexp_ast_java", "yandex_gpt_lite_only_code_java")
-#reports.make_relate("phi_mini_code_w_sexp_ast_python", "phi_mini_only_code_python")
+case_results.execute_case_results()
+# reports.make_total()
+# reports.make_relate("yandex_gpt_lite_code_w_sexp_ast_python", "yandex_gpt_lite_only_code_python")
+# reports.make_relate("yandex_gpt_lite_code_w_sexp_ast_java", "yandex_gpt_lite_only_code_java")
+# reports.make_relate("phi_mini_code_w_sexp_ast_python", "phi_mini_only_code_python")
 reports.make_relate("phi_mini_code_w_sexp_ast_java", "phi_mini_only_code_java")
-
